6f8255d2493201631f058765e3fdfb59d6baadd3
Felix Duvallet <felix.duvallet@epfl.ch>
2016-04-06 08:18:57 +0200

    show backup progress bar.

--- scripts/backup_directories.sh
+++ scripts/backup_directories.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Backup multiple directories into individual archives.
+#
+# NOTE: requires 'pv' tool for progress bar.
 
-dirs_to_backup="/bin /boot /sbin"
+dirs_to_backup="/bin /boot /dev /etc /home /lib /lib32 /lib64 /opt /run /sbin /scratch /srv /sys /usr /var"
 backup_location="/data/"
 
 #for dir in "/bin" "/boot"
@@ -10,5 +12,5 @@ for dir in $dirs_to_backup
 do
     echo "== Backing up ${dir} =="
     base=$(basename "$dir")
-    tar -czf "$backup_location/${base}.tar.gz" "$dir"
+    ionice -c 3 nice tar cf - "$dir" -P | pv -s $(du -sb "$dir" | awk '{print $1}') | ionice -c 3 nice gzip > "$backup_location/${base}.tar.gz"
 done
